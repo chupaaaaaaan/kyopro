183118fcf808b24c4187f2ef9a4d99ddcbc14fa7
Takayuki Uchida <jacobitebeerlove@gmail.com>
2020-07-26 14:45:37 +0900

    solved.

---
+++ atcoder_other/m-solutions-procon/a/Main.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE BangPatterns     #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiWayIf       #-}
+import           Control.Monad
+import           Control.Monad.ST
+import           Control.Monad.State
+import           Control.Monad.Trans.State
+import           Data.Array.IArray
+import           Data.Array.ST
+import           Data.Array.Unboxed        (UArray)
+import qualified Data.Array.Unboxed        as U
+import           Data.Bits
+import           Data.ByteString.Char8     (ByteString)
+import qualified Data.ByteString.Char8     as BS
+import           Data.Char
+import           Data.Graph
+import           Data.Int                  (Int64)
+import           Data.List
+import           Data.Maybe
+import           Data.Proxy
+import           Data.Sequence             (Seq, (<|), (><), (|>))
+import qualified Data.Sequence             as Seq
+import qualified Data.Set                  as S
+import           Data.STRef
+import qualified Data.Vector               as V
+import qualified Data.Vector.Unboxed       as VU
+import           Numeric
+
+main :: IO ()
+main = do
+  x <- readLn :: IO Int
+  print $ if
+    | x >= 400 && x < 600 -> 8
+    | x >= 600 && x < 800 -> 7
+    | x >= 800 && x < 1000 -> 6
+    | x >= 1000 && x < 1200 -> 5
+    | x >= 1200 && x < 1400 -> 4
+    | x >= 1400 && x < 1600 -> 3
+    | x >= 1600 && x < 1800 -> 2
+    | x >= 1800 && x < 2000 -> 1
+
+
+-- converter
+unconsChar :: StateT BS.ByteString Maybe Char
+unconsChar = StateT BS.uncons
+
+unconsInt :: StateT BS.ByteString Maybe Int
+unconsInt = StateT $ BS.readInt . BS.dropWhile isSpace
+
+unconsInteger :: StateT BS.ByteString Maybe Integer
+unconsInteger = StateT $ BS.readInteger . BS.dropWhile isSpace
+
+-- for list
+readLnAsListWith :: StateT BS.ByteString Maybe a -> IO [a]
+readLnAsListWith st = unfoldr (runStateT st) <$> BS.getLine
+
+-- for unboxed vector
+readLnAsUVecNWith :: VU.Unbox a => Int -> StateT BS.ByteString Maybe a -> IO (VU.Vector a)
+readLnAsUVecNWith !n !st = VU.unfoldrN n (runStateT st) <$> BS.getLine
+
+-- for boxed vector
+readLnAsVecNWith :: Int -> StateT BS.ByteString Maybe a -> IO (V.Vector a)
+readLnAsVecNWith !n !st = V.unfoldrN n (runStateT st) <$> BS.getLine
+
+getAsString :: IO [String]
+getAsString = map BS.unpack . BS.words <$> BS.getLine
+
+getAsCharArray1DWithLength :: IO (Int, UArray Int Char)
+getAsCharArray1DWithLength = BS.getLine >>= \ !cs ->
+  let !n = BS.length cs
+      !ary = (U.listArray (1,n) $ unfoldr BS.uncons cs) :: UArray Int Char
+  in return (n, ary)
+
+-- n: number of lines
+-- m: number of chars per a line
+getAsCharArray2D :: Int -> Int -> IO (UArray (Int,Int) Char)
+getAsCharArray2D !n !m = U.listArray ((1,1),(n,m)) . unfoldr BS.uncons . BS.concat <$> replicateM n BS.getLine
