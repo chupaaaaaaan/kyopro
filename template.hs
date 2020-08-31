ebb2bd9531daef1c373afe223e22a86f44f582a8
Takayuki Uchida <jacobitebeerlove@gmail.com>
2020-08-31 15:17:56 +0900

    Unboxed -> Unboxing

--- template.hs
+++ template.hs
@@ -11,22 +11,21 @@ import           Data.Array.IArray
 import           Data.Array.ST
 import           Data.Array.Unboxed
 import           Data.Bits
-import qualified Data.ByteString.Char8       as BS
+import qualified Data.ByteString.Char8        as BS
 import           Data.Char
-import           Data.Int                    (Int64)
 import           Data.List
 import           Data.Maybe
 import           Data.Proxy
-import           Data.Sequence               (Seq ((:<|), (:|>)), ViewL ((:<)),
-                                              ViewR ((:>)), viewl, viewr, (<|),
-                                              (><), (|>))
-import qualified Data.Sequence               as Seq
-import qualified Data.Set                    as S
+import           Data.Sequence                (Seq ((:<|), (:|>)), ViewL ((:<)),
+                                               ViewR ((:>)), viewl, viewr, (<|),
+                                               (><), (|>))
+import qualified Data.Sequence                as Seq
+import qualified Data.Set                     as S
 import           Data.STRef
-import qualified Data.Vector                 as V
-import qualified Data.Vector.Mutable         as VM
-import qualified Data.Vector.Unboxed         as VU
-import qualified Data.Vector.Unboxed.Mutable as VUM
+import qualified Data.Vector                  as V
+import qualified Data.Vector.Mutable          as VM
+import qualified Data.Vector.Unboxing         as VU
+import qualified Data.Vector.Unboxing.Mutable as VUM
 import           Numeric
 
 main :: IO ()
@@ -59,11 +58,11 @@ readLnAsVecWith :: StateT BS.ByteString Maybe a -> Int -> IO (V.Vector a)
 readLnAsVecWith !st !n = V.unfoldrN n (runStateT st) <$> BS.getLine
 
 -- for unboxed vector
-readLnAsUVecWith :: VU.Unbox a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector a)
+readLnAsUVecWith :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector a)
 readLnAsUVecWith !st !n = VU.unfoldrN n (runStateT st) <$> BS.getLine
 
 -- example: 1D tuple vector
-readLnAsUVecWith2Tuple :: VU.Unbox a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector (a,a))
+readLnAsUVecWith2Tuple :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector (a,a))
 readLnAsUVecWith2Tuple !st !n = VU.replicateM n $ (\vec -> (vec VU.! 0, vec VU.! 1)) <$> readLnAsUVecWith st 2
 
 
@@ -75,5 +74,5 @@ readLnAsUVecWith2Tuple !st !n = VU.replicateM n $ (\vec -> (vec VU.! 0, vec VU.!
 readLnAs2DVecWith :: StateT BS.ByteString Maybe a -> Int -> Int -> IO (V.Vector a)
 readLnAs2DVecWith !st !n !m = V.unfoldrN (n * m) (runStateT st) . BS.concat <$> replicateM n BS.getLine
 
-readLnAs2DUVecWith :: VU.Unbox a => StateT BS.ByteString Maybe a -> Int -> Int -> IO (VU.Vector a)
+readLnAs2DUVecWith :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> Int -> IO (VU.Vector a)
 readLnAs2DUVecWith !st !n !m = VU.unfoldrN (n * m) (runStateT st) . BS.concat <$> replicateM n BS.getLine
