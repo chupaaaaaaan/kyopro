7e9404042f46874e9166c02da5b8e11abb105c28
Takayuki Uchida <jacobitebeerlove@gmail.com>
2022-02-10 08:46:13 +0900

    ビルドスクリプト修正

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 
 cd $(dirname $0)
 
+if [ $# -eq 0 ]; then
+    echo "Invalid number of argument: $#"
+    exit 1
+fi
+
 sourceDir="$1"
+
+if [ ! -f ${sourceDir}/Main.hs ]; then
+    echo "${sourceDir}/Main.hs does not exist"
+    exit 1
+fi
 
 ghc -package-env ./package.local -o a.out -O2 ${sourceDir}/Main.hs
 
