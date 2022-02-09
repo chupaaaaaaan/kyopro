b022b4eb6ae9c747aca289fc2423f7d79a1aa93c
Takayuki Uchida <jacobitebeerlove@gmail.com>
2022-02-10 08:33:22 +0900

    セットアップツール修正

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-for problem_path in "$@"; do
+cd $(dirname $0)
 
-    pathConfirm1=$(echo ${problem_path} | tr '/' ' ' | cut -d' ' -f1 | xargs realpath)
-    pathConfirm2=$(pwd)/$(echo ${problem_path} | tr '/' ' ' | cut -d' ' -f1)
-
-    if [ "${pathConfirm1}" != "${pathConfirm2}" ]; then
-        echo "invalid problem path."
-        exit 1
-    fi
-
-    if [ ! -s "${problem_path}/Main.hs" ]; then
-        mkdir -p ${problem_path}
-        cat template.hs > ${problem_path}/Main.hs
-        echo "create file ${problem_path}/Main.hs from template.hs."
+for path in "$@"
+do
+    if [ ! -f "${path}/Main.hs" ]
+    then
+        mkdir -p ${path}
+        cat template.hs > ${path}/Main.hs
+        echo "create file ${path}/Main.hs from template.hs."
     fi
 done
