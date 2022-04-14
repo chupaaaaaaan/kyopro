50177ee046215f57807d296b3a0f8f973a4c02c8
Takayuki Uchida <jacobitebeerlove@gmail.com>
2022-04-14 18:57:10 +0900

    add tool

---
+++ submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+if [ $# -ne 1 ]; then
+    echo "Invalid number of argument: $#"
+    exit 1
+fi
+
+URL=$1
+CODEPATH=${URL##*://}
+MAINHS=${CODEPATH}/Main.hs
+
+if [ ! -f ${MAINHS} ]; then
+    echo "${MAINHS} does not exist."
+    exit 1
+fi
+
+oj s ${URL} ${MAINHS}
