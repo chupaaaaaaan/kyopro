dfeb0896a9fac652a23b9395a503c8c8781a593e
Takayuki Uchida <jacobitebeerlove@gmail.com>
2023-03-29 06:48:28 +0900

    入出力関数の名称変更

--- template.hs
+++ template.hs
@@ -1,90 +1,75 @@
-{-# LANGUAGE BangPatterns               #-}
-{-# LANGUAGE DerivingStrategies         #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiWayIf                 #-}
-{-# LANGUAGE RankNTypes                 #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE TypeApplications           #-}
-{-# LANGUAGE UndecidableInstances       #-}
-import           Control.Monad
-import           Control.Monad.ST
-import           Control.Monad.State.Strict
-import           Data.Bits
-import qualified Data.ByteString.Char8        as BS
-import           Data.Char
-import           Data.List
-import           Data.Maybe
-import           Data.Proxy
-import           Data.Sequence                (Seq ((:<|), (:|>)), ViewL ((:<)),
-                                               ViewR ((:>)), viewl, viewr, (<|),
-                                               (><), (|>))
-import qualified Data.Sequence                as Seq
-import qualified Data.Set                     as S
-import           Data.STRef
-import qualified Data.Vector                  as V
-import qualified Data.Vector.Mutable          as VM
-import qualified Data.Vector.Unboxing         as VU
-import qualified Data.Vector.Unboxing.Mutable as VUM
-import           Numeric
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+import Control.Monad
+import Control.Monad.ST
+import Control.Monad.State.Strict (StateT (..))
+import Data.Bits
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Char
+import qualified Data.List as L
+import Data.Maybe
+import Data.Proxy
+import Data.STRef
+import qualified Data.Sequence as Seq
+import qualified Data.Set as S
+import Data.Vector.Generic (Vector)
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Unboxing as V
+import qualified Data.Vector.Unboxing.Mutable as VM
+import Numeric
 
 main :: IO ()
 main = do
-  s <- readLnAsListWith unconsChar
-  n <- readLn @Int
-  [m,_] <- readLnAsListWith unconsInt
-  as <- readLnAsListWith unconsInt
-  ss <- readLnAsListWith unconsChar
-  avec <- readLnAsUVecWith unconsInt n
-  xyvec <- readLnAsUVecWith2Tuple unconsInt n
+    n <- readLn @Int
+    rowl unconsChar
+    rowl unconsInt
+    rowv @V.Vector n unconsInt
+    colv2 @V.Vector n unconsInt
 
-  return ()
-
-
+    return ()
 
 -- converter
-unconsChar :: StateT BS.ByteString Maybe Char
+unconsChar :: StateT ByteString Maybe Char
 unconsChar = StateT BS.uncons
 
-unconsInt :: StateT BS.ByteString Maybe Int
-unconsInt = StateT $ BS.readInt . BS.dropWhile isSpace
+unconsInt :: StateT ByteString Maybe Int
+unconsInt = StateT (BS.readInt . BS.dropWhile isSpace)
 
-unconsInteger :: StateT BS.ByteString Maybe Integer
-unconsInteger = StateT $ BS.readInteger . BS.dropWhile isSpace
+unconsInteger :: StateT ByteString Maybe Integer
+unconsInteger = StateT (BS.readInteger . BS.dropWhile isSpace)
 
--- 1D Data
--- list
-readLnAsListWith :: StateT BS.ByteString Maybe a -> IO [a]
-readLnAsListWith !st = unfoldr (runStateT st) <$> BS.getLine
+-- read a linear data as List
+rowl :: StateT ByteString Maybe a -> IO [a]
+rowl !st = L.unfoldr (runStateT st) <$> BS.getLine
 
--- boxed vector
-readLnAsVecWith :: StateT BS.ByteString Maybe a -> Int -> IO (V.Vector a)
-readLnAsVecWith !st !n = V.unfoldrN n (runStateT st) <$> BS.getLine
+-- read a row Vector of order n
+rowv :: Vector v a => Int -> StateT ByteString Maybe a -> IO (v a)
+rowv !n !st = VG.unfoldrN n (runStateT st) <$> BS.getLine
 
--- unboxed vector
-readLnAsUVecWith :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector a)
-readLnAsUVecWith !st !n = VU.unfoldrN n (runStateT st) <$> BS.getLine
+-- read a column Vector of order n
+colv :: Vector v a => Int -> StateT ByteString Maybe a -> IO (v a)
+colv !n !st = VG.replicateM n (\[a] -> a) <$> rowl st
 
--- unboxed vector (input for n-lines)
-readLnAsUVecCWith :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector a)
-readLnAsUVecCWith !st !n = VU.replicateM n $ (VU.! 0) <$> readLnAsUVecWith st 1
+-- read a column Vector of order n whose element is 2-tuple
+colv2 :: Vector v (a, a) => Int -> StateT ByteString Maybe a -> IO (v (a, a))
+colv2 !n !st = VG.replicateM n $ (\[a, b] -> (a, b)) <$> rowl st
 
--- unboxed 2-tuple vector
-readLnAsUVecWith2Tuple :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector (a,a))
-readLnAsUVecWith2Tuple !st !n = VU.replicateM n $ (\vec -> (vec VU.! 0, vec VU.! 1)) <$> readLnAsUVecWith st 2
+-- read a column Vector of order n whose element is 3-tuple
+colv3 :: Vector v (a, a, a) => Int -> StateT ByteString Maybe a -> IO (v (a, a, a))
+colv3 !n !st = VG.replicateM n $ (\[a, b, c] -> (a, b, c)) <$> rowl st
 
--- unboxed 3-tuple vector
-readLnAsUVecWith3Tuple :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> IO (VU.Vector (a,a,a))
-readLnAsUVecWith3Tuple !st !n = VU.replicateM n $ (\vec -> (vec VU.! 0, vec VU.! 1, vec VU.! 2)) <$> readLnAsUVecWith st 3
-
--- 2D Data
+-- read a (n * m) grid data as Vector (linear-indexed)
 -- n: number of lines
 -- m: number of values per a line
-
--- boxed vector
-readLnAs2DVecWith :: StateT BS.ByteString Maybe a -> Int -> Int -> IO (V.Vector a)
-readLnAs2DVecWith !st !n !m = V.unfoldrN (n * m) (runStateT st) . BS.concat <$> replicateM n BS.getLine
-
--- unboxed vector
-readLnAs2DUVecWith :: VU.Unboxable a => StateT BS.ByteString Maybe a -> Int -> Int -> IO (VU.Vector a)
-readLnAs2DUVecWith !st !n !m = VU.unfoldrN (n * m) (runStateT st) . BS.concat <$> replicateM n BS.getLine
+-- example: if you want to access the element of (i, j) (0<=i<m, 0<=j<n), the index is i+m*j
+gridv :: Vector v a => Int -> Int -> StateT ByteString Maybe a -> IO (v a)
+gridv !n !m !st = VG.unfoldrN (n * m) (runStateT st) . BS.concat <$> replicateM n BS.getLine
