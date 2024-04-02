d7748bf150932134c8f25c69952a4e7285e0f447
Takayuki Uchida <jacobitebeerlove@gmail.com>
2024-04-03 00:50:27 +0900

    テンプレート修正

--- src/template.hs
+++ src/template.hs
@@ -7,27 +7,29 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 
 import Control.Monad
 import Control.Monad.ST
 import Control.Monad.State.Strict
-import Data.Array.IArray
 import Data.Array.ST
+import Data.Array.Unboxed
 import Data.Bits
 import Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as BS
+import Data.ByteString.Char8 qualified as BS
 import Data.Char
-import qualified Data.List as L
+import Data.Foldable
+import Data.List qualified as L
 import Data.Maybe
 import Data.STRef
-import qualified Data.Sequence as Seq
-import qualified Data.Set as S
+import Data.Sequence qualified as Seq
+import Data.Set qualified as S
 import Data.Vector (Vector)
-import qualified Data.Vector as V
+import Data.Vector qualified as V
 import Data.Vector.Algorithms.Intro
 import Data.Vector.Mutable (MVector)
-import qualified Data.Vector.Mutable as VM
-
+import Data.Vector.Mutable qualified as VM
+import Debug.Trace
 import Numeric
 
 main :: IO ()
@@ -39,50 +41,156 @@ main = do
 
 -- Input
 -- converter
-ucChar :: StateT ByteString Maybe Char
+type Conv = StateT ByteString Maybe
+
+ucChar :: Conv Char
 ucChar = StateT BS.uncons
 
-ucInt :: StateT ByteString Maybe Int
+ucInt :: Conv Int
 ucInt = StateT (BS.readInt . BS.dropWhile isSpace)
 
-ucString :: StateT ByteString Maybe ByteString
+ucString :: Conv ByteString
 ucString = StateT (\bs -> let bs' = BS.dropWhile isSpace bs
                           in if BS.null bs'
                              then Nothing
                              else Just $ BS.break isSpace bs')
 
 -- | read a linear data as List
-list1 ::
-    -- | converter
-    StateT ByteString Maybe a ->
-    IO [a]
+list1 :: Conv a -> IO [a]
 list1 !st = L.unfoldr (runStateT st) <$> BS.getLine
 
 -- | read multi line data as List
-list2 ::
-    -- | number of lines (height)
-    Int ->
-    -- | converter
-    StateT ByteString Maybe a ->
-    IO [[a]]
+list2 :: Int -> Conv a -> IO [[a]]
 list2 !n !st = fmap (L.unfoldr (runStateT st)) <$> replicateM n BS.getLine
 
--- | convert singleton List to Vector
-toVec1 :: [[a]] -> Vector a
-toVec1 = V.fromList . fmap (\[a] -> a)
+-- | read a line and convert to Vector
+vector1 :: Int -> Conv a -> IO (Vector a)
+vector1 n !st = V.unfoldrN n (runStateT st) <$> BS.getLine
 
--- | read a column Vector of order n whose element is 2-tuple
-toVect :: [[a]] -> Vector (a, a)
-toVect = V.fromList . fmap (\[a, b] -> (a, b))
+toIntGrid :: (Ix a, Ix b) => ((a, b), (a, b)) -> [[Int]] -> UArray (a, b) Int
+toIntGrid b = listArray b . L.concat
 
--- | read a (h * w) grid data as Array
--- We assume that 2d-index starts from (1,1)
-grid ::
-    -- | number of lines (height)
-    Int ->
-    -- | number of values per a line (width)
-    Int ->
-    -- | converter
-    StateT ByteString Maybe a ->
-    IO (Array (Int, Int) a)
-grid h w st = listArray ((1, 1), (h, w)) . concat <$> list2 h st
+-- Array
+{-# INLINE modifyArray #-}
+modifyArray :: (MArray a e m, Ix i) => a i e -> i -> (e -> e) -> m ()
+modifyArray marr i f = readArray marr i >>= writeArray marr i . f
+
+-- Cumulative Sum
+-- | Array用のscanl
+ascanl :: (MArray ma e1 m, IArray ia e2) => (e1 -> e2 -> e1) -> e1 -> ia Int e2 -> m (ma Int e1)
+ascanl f a arr = do
+    let b@(l,h) = bounds arr
+
+    result <- newArray_ (l-1,h)
+
+    writeArray result (l-1) a
+
+    for_ (range b) $ \i -> do
+        x <- readArray result (i-1)
+        let y = arr ! i
+        writeArray result i (x `f` y)
+
+    return result
+
+-- | Array用のscanl1
+ascanl1 :: (MArray ma e m, IArray ia e) => (e -> e -> e) -> ia Int e -> m (ma Int e)
+ascanl1 f arr = do
+    let b@(l,h) = bounds arr
+
+    result <- newArray_ (l,h)
+
+    writeArray result l (arr ! l)
+    for_ (range (l+1,h)) $ \i -> do
+        x <- readArray result (i-1)
+        let y = arr ! i
+        writeArray result i (x `f` y)
+
+    return result
+
+-- | Array用のscanr
+ascanr :: (MArray ma e1 m, IArray ia e2) => (e2 -> e1 -> e1) -> e1 -> ia Int e2 -> m (ma Int e1)
+ascanr f a arr = do
+    let b@(l,h) = bounds arr
+
+    result <- newArray_ (l,h+1)
+
+    writeArray result (h+1) a
+
+    for_ (reverse $ range b) $ \i -> do
+        let x = arr ! i
+        y <- readArray result (i+1)
+        writeArray result i (x `f` y)
+
+    return result
+
+
+-- | Array用のscanr
+ascanr1 :: (MArray ma e m, IArray ia e) => (e -> e -> e) -> ia Int e -> m (ma Int e)
+ascanr1 f arr = do
+    let b@(l,h) = bounds arr
+
+    result <- newArray_ (l,h)
+
+    writeArray result h (arr ! h)
+
+    for_ (reverse $ range (l,h-1)) $ \i -> do
+        let x = arr ! i
+        y <- readArray result (i+1)
+        writeArray result i (x `f` y)
+
+    return result
+
+
+-- | 2次元Array用のscanl
+ascanl2d :: (MArray ma e m, IArray ia e) => (e -> e -> e) -> e -> ia (Int, Int) e -> m (ma (Int, Int) e)
+ascanl2d f a arr = do
+    let b@((li,lj),(hi,hj)) = bounds arr
+        bx = ((li-1,lj-1),(hi,hj))
+
+    result <- newArray bx a
+
+    for_ (range b) $ \(i,j) -> 
+        writeArray result (i,j) $ arr ! (i,j)
+
+    for_ [(i,j) | i <- [li..hi], j <- [lj..hj]] $ \(i,j) -> do
+        x <- readArray result (i-1,j)
+        y <- readArray result (i,j)
+        writeArray result (i,j) (x `f` y)
+
+    for_ [(i,j) | i <- [li..hi], j <- [lj..hj]] $ \(i,j) -> do
+        x <- readArray result (i,j-1)
+        y <- readArray result (i,j)
+        writeArray result (i,j) (x `f` y)
+
+    return result
+
+-- | 2次元Array用のscanl1
+ascanl2d1 :: (MArray ma e m, IArray ia e) => (e -> e -> e) -> ia (Int, Int) e -> m (ma (Int, Int) e)
+ascanl2d1 f arr = do
+    let b@((li,lj),(hi,hj)) = bounds arr
+
+    result <- newArray_ b
+
+    for_ (range b) $ \(i,j) -> 
+        writeArray result (i,j) $ arr ! (i,j)
+
+    for_ [(i,j) | i <- [li+1..hi], j <- [lj..hj]] $ \(i,j) -> do
+        x <- readArray result (i-1,j)
+        y <- readArray result (i,j)
+        writeArray result (i,j) (x `f` y)
+
+    for_ [(i,j) | i <- [li..hi], j <- [lj+1..hj]] $ \(i,j) -> do
+        x <- readArray result (i,j-1)
+        y <- readArray result (i,j)
+        writeArray result (i,j) (x `f` y)
+
+    return result
+
+
+-- | 2次元累積和から矩形領域の和を計算する際の抽象
+rectangleSum :: (Ix a, Ix b, Num a, Num b, Num c) => ((a, b) -> c) -> (a, b) -> (a, b) -> c
+rectangleSum f (a,b) (c,d) = f (c,d) + f (a-1,b-1) - f (a-1,d) - f (c,b-1)
+
+-- | 2次元累積和をクエリする
+query2dCS :: (IArray a e, Num e) => a (Int, Int) e -> (Int, Int) -> (Int, Int) -> e
+query2dCS csArray = rectangleSum (csArray !)
