5835e63921b54fc663cad4ea3cff3aef6775a2b4
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-04-26 04:28:36 +0900

    素数の計算の改善

--- src/My/Math/Prime.hs
+++ src/My/Math/Prime.hs
@@ -1,12 +1,8 @@
-module My.Math.Prime
-    ( primes
-    , sieve
-    , factorize
-    , divisors
-    , isPrime
-    ) where
+module My.Math.Prime where
 
+import Control.Applicative
 import Control.Monad
+import qualified Data.List as L
 import qualified Data.Vector.Unboxing as VU
 import qualified Data.Vector.Unboxing.Mutable as VUM
 
@@ -19,17 +15,19 @@ primes n = let sv = sieve n
 -- | 添字の数字の最初の素因数（Smallest Prime Factor, SPF）を格納した配列を作る(Eratosthenesの篩)
 -- もし添字の数字と格納されている数が等しければ、素数
 sieve :: Int -> VU.Vector Int
-sieve n = VU.create $ do
-    vec <- VUM.replicate (n+1) 0
-    VUM.write vec 1 1
-    forM_ [2..n] $ \i -> do
-        b <- VUM.read vec i
-        when (b == 0) $ do
-            VUM.write vec i i
-            forM_ [i*i,(i+1)*i..n] $ \j -> do
-                c <- VUM.read vec j
-                when (c == 0) $ VUM.write vec j i
-    return vec
+sieve n
+    | n < 1 = VU.empty
+    | otherwise = VU.create $ do
+          vec <- VUM.replicate (n+1) 0
+          VUM.write vec 1 1
+          forM_ [2..n] $ \i -> do
+              b <- VUM.read vec i
+              when (b == 0) $ do
+                  VUM.write vec i i
+                  when (i <= n `div` i) $ forM_ [i*i,(i+1)*i..n] $ \j -> do
+                      c <- VUM.read vec j
+                      when (c == 0) $ VUM.write vec j i
+          return vec
 
 
 type Prime = Int
@@ -47,19 +45,33 @@ factorize sv n = go 1 (sv VU.! n) (n `div` sv VU.! n)
                                then go (a+1) p (m`div`m')
                                else (p, a) : go 1 m' (m`div`m')
 
--- 与えられた整数の約数を全列挙する（O(\sqrt n)）
-divisors :: Int -> [Int]
-divisors n = go 1
+-- 素因数分解のリストから、与えられた数の約数を全列挙する
+divisors :: [(Prime,Factor)] -> [Int]
+divisors = L.foldl' (liftA2 (*)) [1] . map (\(p,f) -> map (p ^) [0..f])
+
+-- 予め作っておいたSPF配列を渡して、約数を全列挙する
+divisors' :: VU.Vector Int -> Int -> [Int]
+divisors' sv = divisors . factorize sv
+
+-- 与えられた数の約数を全列挙する( $O(\sqrt{n})$ )
+divisors'' :: Int -> [Int]
+divisors'' n = go 1
     where go f | f * f > n = []
                | f * f == n = [f]
                | otherwise = if n`mod`f == 0
                              then (n`div`f) : f : go (f+1)
                              else go (f+1)
 
--- 与えられた整数が素数かを判定する（O(\sqrt n)）
-isPrime :: Int -> Bool
-isPrime n
-    | abs n < 2 = False
+-- 予め作っておいたSPF配列を渡して、与えられた数が素数かを判定する
+isPrime :: VU.Vector Int -> Int -> Bool
+isPrime sv n
+    | n < 2 = False
+    | otherwise = sv VU.! n == n
+
+-- 与えられた数が素数かを判定する( $O(\sqrt{n})$ )
+isPrime' :: Int -> Bool
+isPrime' n
+    | n < 2 = False
     | otherwise = go 2
     where go f | f * f > n = True
                | otherwise = ((n`mod`f) /= 0) && go (f+1)
