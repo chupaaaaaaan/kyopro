143618923ad5612c5a709f759ce1f5d2c5e61472
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-05 00:44:57 +0900

    QuickCheck用生成関数追加

--- src/My/Test/QuickCheck.hs
+++ src/My/Test/QuickCheck.hs
@@ -4,11 +4,20 @@ import qualified Data.List as L
 import qualified Test.QuickCheck as QC
 
 -- Generators
-
-genPositiveStrictlyIncreasingList :: QC.Gen [Int]
-genPositiveStrictlyIncreasingList = do
+-- | Positive-Strictly-Monotomically-Increasing (PSMI) List
+genPSMIList :: QC.Gen [Int]
+genPSMIList = do
     start <- QC.getPositive <$> QC.arbitrary
     L.scanl' (+) start <$> genPositiveList
 
+-- | Positive-Broadly-Monotomically-Increasing (PBMI) List
+genPBMIList :: QC.Gen [Int]
+genPBMIList = do
+    start <- QC.getPositive <$> QC.arbitrary
+    L.scanl' (+) start <$> genNonNegativeList
+
 genPositiveList :: QC.Gen [Int]
 genPositiveList = fmap QC.getPositive . QC.getNonEmpty <$> QC.arbitrary
+
+genNonNegativeList :: QC.Gen [Int]
+genNonNegativeList = fmap QC.getNonNegative . QC.getNonEmpty <$> QC.arbitrary
