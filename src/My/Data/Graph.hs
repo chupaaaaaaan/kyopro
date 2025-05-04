ee960d357ae000f6264e8d49ff3750ed40b31997
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-05 00:32:09 +0900

    辺の向きを逆にした有向グラフの作成処理追加

--- src/My/Data/Graph.hs
+++ src/My/Data/Graph.hs
@@ -1,12 +1,13 @@
 module My.Data.Graph where
 
 import Data.Array.IArray
+import Data.Tuple
 
 type Graph i = Array i [i]
 type WGraph i a = Array i [(i, a)]
 
 -- | 隣接リスト形式の重み付きグラフを生成する
-genWGraph, genWDiGraph :: Ix i =>
+genWGraph, genWDiGraph, genWInvDiGraph :: Ix i =>
     (i, i) ->      -- ^ 頂点の範囲
     [(i, i, a)] -> -- ^ 辺のリスト (開始, 終了, 重み)
     WGraph i a
@@ -17,8 +18,11 @@ genWGraph b = accumArray (flip (:)) [] b . concatMap (\(p,q,w) -> [(p,(q,w)), (q
 -- | 有向グラフ
 genWDiGraph b = accumArray (flip (:)) [] b . map (\(p,q,w) -> (p,(q,w)))
 
+-- | 逆向きの有向グラフ
+genWInvDiGraph b = accumArray (flip (:)) [] b . map (\(p,q,w) -> (q,(p,w)))
+
 -- | 隣接リスト形式の重みなしグラフを生成する
-genGraph, genDiGraph :: Ix i =>
+genGraph, genDiGraph, genInvDiGraph :: Ix i =>
     (i, i) ->   -- ^ 頂点の範囲
     [(i, i)] -> -- ^ 辺のリスト (開始, 終了)
     Graph i
@@ -28,3 +32,6 @@ genGraph b = accumArray (flip (:)) [] b . concatMap (\(p,q)->[(p,q),(q,p)])
 
 -- | 有向グラフ
 genDiGraph = accumArray (flip (:)) []
+
+-- | 逆向きの有向グラフ
+genInvDiGraph b = accumArray (flip (:)) [] b . map swap
