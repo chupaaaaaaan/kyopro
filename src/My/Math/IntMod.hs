26c6751e7c545617bf06bee38d45857d77d46a5c
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-19 14:08:08 +0900

    効率的なnCkModの計算を実装

--- src/My/Math/IntMod.hs
+++ src/My/Math/IntMod.hs
@@ -28,15 +28,51 @@ powMod !x !k
   | even k = powMod (x * x) (k `div` 2)
   | otherwise = x * powMod (x * x) (k `div` 2)
 
-invMod :: IntMod -> IntMod
-invMod 0  = error "inverse of 0"
-invMod !x = powMod x (modulus - 2)
+data NCKTable =
+    NCK { size :: Int
+        , factTable :: VU.Vector IntMod
+        , invTable :: VU.Vector IntMod
+        , factInvTable :: VU.Vector IntMod
+        }
+
+initNCK :: Int -> NCKTable
+initNCK n = let ft = VU.constructN n $ \vec ->
+                    case VU.length vec of
+                        0 -> 1
+                        1 -> 1
+                        x -> IntMod x * vec VU.! (x - 1)
+                it = VU.constructN n $ \vec ->
+                    case VU.length vec of
+                        0 -> 1
+                        1 -> 1
+                        x -> - vec VU.! (modulus `mod` x) * IntMod (modulus `div` x)
+                fit = VU.constructN n $ \vec ->
+                    case VU.length vec of
+                        0 -> 1
+                        1 -> 1
+                        x -> vec VU.! (x - 1) * it VU.! x
+            in NCK n ft it fit
+
+nCkMod :: NCKTable -> Int -> Int -> IntMod
+nCkMod (NCK size factTable _ factInvTable) n k
+    | size < n || size < k = error "too large arguments"
+    | n < k = error "invalid n and k: n < k"
+    | otherwise = factTable VU.! n * factInvTable VU.! k * factInvTable VU.! (n-k)
+
+nCkMod2 :: NCKTable -> Int -> Int -> IntMod
+nCkMod2 (NCK size _ _ factInvTable) n k
+    | size < n || size < k = error "too large arguments"
+    | n < k = error "invalid n and k: n < k"
+    | otherwise = nPkMod n k * factInvTable VU.! k
+
+invMod :: NCKTable -> Int -> IntMod
+invMod _ 0  = error "inverse of 0"
+invMod (NCK size _ invTable _) n
+    | size < n = error "too large arguments"
+    | otherwise = invTable VU.! n
 
 nPkMod :: Int -> Int -> IntMod
 nPkMod !n' !k' = go 1 n' k'
   where go :: IntMod -> Int -> Int -> IntMod
         go !a !_ 0  = a
         go !a !n !k = go (a * fromIntegral n) (n - 1) (k - 1)
-
-nCkMod :: Int -> Int -> IntMod
-nCkMod !n !k = nPkMod n k * invMod (nPkMod k k)
