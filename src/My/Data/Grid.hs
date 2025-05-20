b551ca0270630eb3f8fb03ab8283aa425a847a03
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-21 02:08:56 +0900

    不要なforallを削除

--- src/My/Data/Grid.hs
+++ src/My/Data/Grid.hs
@@ -13,13 +13,17 @@ nei4 :: (Int, Int) -> [(Int, Int)]
 nei4 (i, j) = [(i, j) ^+ ((p+q)`div`2,(p-q)`div`2) | p <- [-1,1], q <- [-1,1]]
 
 -- | (i, j) から上下左右斜め8つの位置を列挙する
-nei8 :: (Ix i, Num i) => (i, i) -> [(i, i)]
+-- >>> nei8 (0,0)
+-- [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+nei8 :: (Int, Int) -> [(Int, Int)]
 nei8 (i, j) = [bimap (i+) (j+) (p,q) | p <- [-1,0,1], q <- [-1,0,1], (p,q) /= (0,0)]
 
 -- | (i, j) からマンハッタン距離がd以下である位置を列挙する
-neiM :: (Ix i, Num i, Enum i) => i -> (i, i) -> [(i, i)]
+-- >>> neiM 2 (0,0)
+-- [(-2,0),(-1,-1),(-1,0),(-1,1),(0,-2),(0,-1),(0,0),(0,1),(0,2),(1,-1),(1,0),(1,1),(2,0)]
+neiM :: Int -> (Int, Int) -> [(Int, Int)]
 neiM d (i, j) = [bimap (i+) (j+) (p,q) | p <- [-d..d], q <- [-d..d], abs p + abs q <= d]
 
 -- | 探索候補点を列挙する
-nexts :: forall i. Ix i => (i -> Bool) -> (i, i) -> (i -> [i]) -> i -> [i]
+nexts :: Ix i => (i -> Bool) -> (i, i) -> (i -> [i]) -> i -> [i]
 nexts isCand bound neighbours = filter isCand . filter (inRange bound) . neighbours
