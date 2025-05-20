4c7203b3f31423f7b533191fd5803bc71d391d4a
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-21 02:09:33 +0900

    グリッドの方向を表すデータ型を追加

--- src/My/Data/Grid.hs
+++ src/My/Data/Grid.hs
@@ -6,6 +6,27 @@ import Data.Bifunctor
 genGrid :: (IArray a e, Ix i, Ix j) => ((i, j), (i, j)) -> [[e]] -> a (i, j) e
 genGrid b = listArray b . concat
 
+data Direction = L | R | U | D
+    deriving (Show, Read, Eq, Bounded, Enum)
+
+allDirections :: [Direction]
+allDirections = [minBound..maxBound]
+
+d2c :: Direction -> Char
+d2c L = '<'
+d2c R = '>'
+d2c U = '^'
+d2c D = 'v'
+
+d2i :: Direction -> (Int, Int)
+d2i L = (0,-1)
+d2i R = (0,1)
+d2i U = (-1,0)
+d2i D = (1,0)
+
+(^+) :: (Int, Int) -> (Int, Int) -> (Int, Int)
+(i, j) ^+ (u, v) = (i+u, j+v)
+
 -- | (i, j) から上下左右4つの位置を列挙する
 -- >>> nei4 (0,0)
 -- [(-1,0),(0,-1),(0,1),(1,0)]
