7034ce828948ff82313fcedb87af50c90c01b70d
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-22 01:12:07 +0900

    トポロジカルソートを実装

--- src/My/Algorithm/DFS.hs
+++ src/My/Algorithm/DFS.hs
@@ -28,3 +28,27 @@ dfs nexts b start = do
 
     -- 訪問状態を返却
     return seen
+
+-- | DAGをトポロジカルソートした頂点リストを返す
+-- DAGでないグラフを与えることは想定していない
+-- ex. onGraph: let dist = topologicalSort (adj graph) (bounds graph)
+topologicalSort :: forall i. Ix i =>
+    (i -> [i]) -> -- ^ 現在点から探索候補点を取得
+    (i, i) ->     -- ^ 探索範囲のbound
+    [i]
+topologicalSort nexts b = runST $ do
+
+    -- 訪問済みかを管理するarrayを作成
+    seen <- newUArray b False
+
+    order <- newRef []
+
+    -- 探索開始
+    for_ (range b) $ \s -> flip fix s $ \loop v -> do
+        t <- readArray seen v
+        if t then return () else do
+            writeArray seen v True
+            for_ (nexts v) loop
+            modifyRef order (v:)
+
+    readRef order
