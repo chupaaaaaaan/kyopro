b6a7b0fba38171702d89c66304c1de236ff2bdfb
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-05-31 00:21:56 +0900

    変数名変更

--- src/My/Algorithm/BFS.hs
+++ src/My/Algorithm/BFS.hs
@@ -7,13 +7,13 @@ import My.Data.Queue
 
 -- | 幅優先探索
 -- ex. onGraph: dist <- bfs @IOUArray (adj graph) (bounds graph) [1]
--- ex. onGrid:  dist <- bfs @IOUArray (candidates ((/='#').(g!)) bnd nei4) bnd [(1,1)]
+-- ex. onGrid:  dist <- bfs @IOUArray (nexts ((/='#').(g!)) bnd nei4) bnd [(1,1)]
 bfs :: (MArray a Int m, Ix i) =>
     (i -> [i]) -> -- ^ 現在点から探索候補点を取得
     (i, i) ->     -- ^ 探索範囲のbound
     [i] ->        -- ^ 開始点
     m (a i Int)
-bfs nexts b start = do
+bfs nextStatus b start = do
     -- 開始点からの距離を格納するarrayを作成（-1は訪れていないことを表す）
     dist <- newArray b (-1)
 
@@ -26,7 +26,7 @@ bfs nexts b start = do
             EmptyQ -> return ()
             v :@ rest -> do
                 d <- readArray dist v
-                candidates <- filterM (fmap (== (-1)) . readArray dist) (nexts v)
+                candidates <- filterM (fmap (== (-1)) . readArray dist) (nextStatus v)
                 forM_ candidates $ \cand -> writeArray dist cand (d+1)
                 loop (rest `addListQ` candidates)
 
