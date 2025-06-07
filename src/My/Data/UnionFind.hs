3f2cbe5b2d20e770356a3f8d6aea5a4ff39453d0
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-06-08 01:11:14 +0900

    Union-Findで使用するデータ構造をdata型宣言にまとめた

--- src/My/Data/UnionFind.hs
+++ src/My/Data/UnionFind.hs
@@ -3,31 +3,36 @@ module My.Data.UnionFind where
 import Data.Array.MArray
 import My.Data.Array
 
+data UnionFind a b i =
+    UF { parentUF :: a i (Maybe i)
+       , sizeUF :: b i Int
+       }
+
 -- | Union-Find木 配列の初期化
-ufInit :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => (i, i) -> m (a i (Maybe i), b i Int)
-ufInit r = do
-    parent <- newBArray r Nothing
-    size <- newUArray r 1
-    return (parent, size)
+ufInit :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => (i, i) -> m (UnionFind a b i)
+ufInit r = UF <$> newBArray r Nothing <*> newUArray r 1
 
 -- | Union-Find木 親を求める
 -- parentの要素のうち、Nothingのものが親となる
-ufRoot :: (MBArray a (Maybe i) m, Ix i) => a i (Maybe i) -> i -> m i
-ufRoot parent v = do
+ufRoot :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => UnionFind a b i -> i -> m i
+ufRoot uf v = do
+    let parent = parentUF uf
     p <- readArray parent v
     case p of
         Nothing -> return v
         Just p' -> do
-            q <- ufRoot parent p'
+            q <- ufRoot uf p'
             writeArray parent v (Just q)
             return q
 
 -- | Union-Find木 グループを統合する
-ufUnite :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => b i Int -> a i (Maybe i) -> i -> i -> m ()
-ufUnite size parent u v = do
-    rootU <- ufRoot parent u
-    rootV <- ufRoot parent v
+ufUnite :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => UnionFind a b i -> i -> i -> m ()
+ufUnite uf u v = do
+    rootU <- ufRoot uf u
+    rootV <- ufRoot uf v
     if rootU == rootV then return () else do
+        let size = sizeUF uf
+            parent = parentUF uf
         sizeU <- readArray size rootU
         sizeV <- readArray size rootV
         if sizeU < sizeV
@@ -37,9 +42,9 @@ ufUnite size parent u v = do
                     writeArray size rootU (sizeU + sizeV)
 
 -- | Union-Find木 同じグループにいるかクエリする
-ufSame :: (MBArray a (Maybe i) m, Ix i) => a i (Maybe i) -> i -> i -> m Bool
-ufSame parent u v = (==) <$> ufRoot parent u <*> ufRoot parent v
+ufSame :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => UnionFind a b i -> i -> i -> m Bool
+ufSame uf u v = (==) <$> ufRoot uf u <*> ufRoot uf v
 
 -- | Union-Find木 ある点が属するグループのサイズを求める
-ufSize :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => b i Int -> a i (Maybe i) -> i -> m Int
-ufSize size parent v = ufRoot parent v >>= readArray size
+ufSize :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => UnionFind a b i -> i -> m Int
+ufSize uf v = ufRoot uf v >>= readArray (sizeUF uf)
