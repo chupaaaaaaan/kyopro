c3c4a85b2c0df689142ae46ddf202b72ebc6f433
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-06-19 21:50:52 +0900

    10進を2進リストに変換する関数追加

--- src/My/Math/Util.hs
+++ src/My/Math/Util.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 module My.Math.Util where
+
+import Data.Foldable
 import My.Algorithm.BinarySearch
 
 
@@ -38,8 +40,26 @@ extgcd = go 1 0 0 1
 -- [(1,1),(2,1),(3,1),(4,2),(5,2),(6,2),(7,2),(8,2),(9,3),(10,3),(11,3),(12,3),(13,3),(14,3),(15,3),(16,4)]
 isqrt :: Integral a => a -> a
 isqrt n
-    | n < 0 = error "Arg must be positive"
+    | n < 0 = error "Arg must not be negative"
     | n < 2 = n
     | otherwise = go $ n `div` 2
     where go x = let x' = (x + n `div` x) `div` 2
                  in if x' >= x then x else go x'
+
+-- | 非負整数を逆順で2進表示したリストを返す
+-- >>> be 187
+-- [1,1,0,1,1,1,0,1]
+be :: Integral a => a -> [a]
+be x
+ | x == 0 = [0]
+ | x > 0  = map (`mod`2) . takeWhile (>0) . iterate (`div`2) $ x
+ | otherwise = error "Arg must not be negative"
+
+-- | 逆順2進表示リストから非負整数を返す
+-- >>> de [1,1,0,1,1,1,0,1]
+-- 187
+de :: forall a. Integral a => [a] -> a
+de x = let d = all (\i -> i == 0 || i == 1) x
+       in if d
+          then foldr' (\ !i !acc -> i + 2 * acc) 0 x
+          else error "List must not contain anything other than 0 or 1"
