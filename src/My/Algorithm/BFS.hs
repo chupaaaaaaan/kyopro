d8b45dbb682a013222154e6ab078c6aceae4703f
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-06-19 22:28:32 +0900

    bfsの戻り値の型を、MArrayからIArrayに変更

--- src/My/Algorithm/BFS.hs
+++ src/My/Algorithm/BFS.hs
@@ -2,23 +2,27 @@ module My.Algorithm.BFS where
 
 import Control.Monad
 import Control.Monad.Fix
-import Data.Array.MArray
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.Foldable
+import My.Data.Array
 import My.Data.Queue
 
--- | 幅優先探索
--- ex. onGraph: dist <- bfs @IOUArray (adj graph) (bounds graph) [1]
--- ex. onGrid:  dist <- bfs @IOUArray (nexts ((/='#').(g!)) bnd nei4) bnd [(1,1)]
-bfs :: (MArray a Int m, Ix i) =>
+-- | 幅優先探索の雛形
+-- この実装では、開始点からの最小距離を管理する配列を返す
+-- ex. onGraph: bfs (adj graph) (bounds graph) [1]
+-- ex. onGrid:  bfs (nexts ((/='#').(g!)) bnd nei4) bnd [(1,1)]
+bfs :: Ix i =>
     (i -> [i]) -> -- ^ 現在点から探索候補点を取得
     (i, i) ->     -- ^ 探索範囲のbound
     [i] ->        -- ^ 開始点
-    m (a i Int)
-bfs nextStatus b start = do
+    UArray i Int
+bfs nextStatus b start = runSTUArray $ do
     -- 開始点からの距離を格納するarrayを作成（-1は訪れていないことを表す）
-    dist <- newArray b (-1)
+    dist <- newUArray b (-1)
 
     -- 開始点に距離0を設定
-    forM_ start $ flip (writeArray dist) 0
+    for_ start $ flip (writeArray dist) 0
 
     -- 探索実施
     flip fix (fromListQ start) $ \loop queue -> do
@@ -27,7 +31,7 @@ bfs nextStatus b start = do
             v :@ rest -> do
                 d <- readArray dist v
                 candidates <- filterM (fmap (== (-1)) . readArray dist) (nextStatus v)
-                forM_ candidates $ \cand -> writeArray dist cand (d+1)
+                for_ candidates $ \cand -> writeArray dist cand (d+1)
                 loop (rest `addListQ` candidates)
 
     -- 開始点からの距離を返却
