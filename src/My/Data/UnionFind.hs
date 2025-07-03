aec198a513e3fe243b88d9e6d4a95f920bebf472
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-07-03 23:57:15 +0900

    UnionFindのparentをMUArrayに変更
    参考
    https://byorgey.github.io/blog/posts/2024/11/02/UnionFind.html

--- src/My/Data/UnionFind.hs
+++ src/My/Data/UnionFind.hs
@@ -5,37 +5,36 @@ import Data.Array.MArray
 import My.Data.Array
 import Control.Monad.Reader
 
-data UnionFind a b i =
-    UF { parentUF :: a i (Maybe i)
-       , sizeUF :: b i Int
+data UnionFind a i =
+    UF { parentUF :: a i i
+       , sizeUF :: a i Int
        }
 
-type RUF a b i m = ReaderT (UnionFind a b i) m
+type RUF a i m = ReaderT (UnionFind a i) m
 
-runUF :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => (i, i) -> RUF a b i m t -> m t
+runUF :: (MUArray a i m, MUArray a Int m, Ix i) => (i, i) -> RUF a i m t -> m t
 runUF bnds act = do
     uf <- ufInit bnds
     runReaderT act uf
 
 -- | Union-Find木 配列の初期化
-ufInit :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => (i, i) -> m (UnionFind a b i)
-ufInit bnds = UF <$> newBArray bnds Nothing <*> newUArray bnds 1
+ufInit :: (MUArray a i m, MUArray a Int m, Ix i) => (i, i) -> m (UnionFind a i)
+ufInit bnds = UF <$> newUListArray bnds (range bnds) <*> newUArray bnds 1
 
 -- | Union-Find木 親を求める
 -- parentの要素のうち、Nothingのものが親となる
-ufRoot :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => i -> RUF a b i m i
+ufRoot :: (MUArray a i m, MUArray a Int m, Ix i) => i -> RUF a i m i
 ufRoot v = do
     uf <- ask
     let parent = parentUF uf
-    lift (readArray parent v) >>= \case
-        Nothing -> return v
-        Just u -> do
-            q <- ufRoot u
-            lift $ writeArray parent v (Just q)
-            return q
+    p <- lift (readArray parent v)
+    if p == v then return v else do
+        q <- ufRoot p
+        lift $ writeArray parent v q
+        return q
 
 -- | Union-Find木 グループを統合する
-ufUnite :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => i -> i -> RUF a b i m ()
+ufUnite :: (MUArray a i m, MUArray a Int m, Ix i) => i -> i -> RUF a i m ()
 ufUnite u v = do
     uf <- ask
     ru <- ufRoot u
@@ -46,18 +45,17 @@ ufUnite u v = do
         su <- lift $ readArray size ru
         sv <- lift $ readArray size rv
         if su < sv
-            then do lift $ writeArray parent ru (Just rv)
+            then do lift $ writeArray parent ru rv
                     lift $ writeArray size rv (su + sv)
-            else do lift $ writeArray parent rv (Just ru)
+            else do lift $ writeArray parent rv ru
                     lift $ writeArray size ru (su + sv)
 
 -- | Union-Find木 同じグループにいるかクエリする
-ufSame :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => i -> i -> RUF a b i m Bool
+ufSame :: (MUArray a i m, MUArray a Int m, Ix i) => i -> i -> RUF a i m Bool
 ufSame u v = (==) <$> ufRoot u <*> ufRoot v
 
 -- | Union-Find木 ある点が属するグループのサイズを求める
-ufSize :: (MBArray a (Maybe i) m, MUArray b Int m, Ix i) => i -> RUF a b i m Int
+ufSize :: (MUArray a i m, MUArray a Int m, Ix i) => i -> RUF a i m Int
 ufSize v = do
     size <- asks sizeUF
-    root <- ufRoot v
-    lift $ readArray size root
+    ufRoot v >>= (lift . readArray size)
