6b4f66f2da693baf9871c32aafdc56e67c7df9ce
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-08-22 01:30:53 +0900

    条件付きデバッグ関数追加

--- src/My/Debug.hs
+++ src/My/Debug.hs
@@ -8,26 +8,35 @@ import qualified Data.List as L
 
 
 dbg :: String -> ()
-dbgS :: Show a => a -> ()
+dbgWhen :: Bool -> String -> ()
+dbgS :: Show a => a -> String -> ()
+dbgSWhen :: Show a => Bool -> a -> String -> ()
 dbgGrid :: (IArray a e, Show e) => a (Int, Int) e -> ()
+dbgGridWhen :: (IArray a e, Show e) => Bool -> a (Int, Int) e -> ()
 
 #ifndef JUDGE
 
 dbg = (`trace`())
+dbgWhen p msg = if p then dbg msg else ()
 
-dbgS = (`traceShow`())
+dbgS x s = if null s then traceShow x () else trace (s <> show x) ()
+dbgSWhen p x s = if p then dbgS x s else ()
 
 dbgGrid grid =
     let ((_,s),(_,e)) = bounds grid
         f xs = if null xs then Nothing else Just $ L.splitAt (e-s+1) xs
     in dbg . unlines . map (L.intercalate "\t" . map show) . L.unfoldr f . elems $ grid
+dbgGridWhen p g = if p then dbgGrid g else ()
 
 #else
 
 dbg = const ()
+dbgWhen _ = const ()
 
-dbgS = const ()
+dbgS _ = const ()
+dbgSWhen _ _ = const ()
 
 dbgGrid = const ()
+dbgGridWhen _ = const ()
 
 #endif
