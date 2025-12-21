b7984f397b8cdf892043d78b1db1a13293416ea4
Takayuki Uchida <jacobitebeerlove@gmail.com>
2025-12-22 02:07:33 +0900

    キューの実装をブラッシュアップ
    - データ構造を正格化
    - pushQの引数順序を変更
    - pushQの演算子版を追加（引数順序をpushQとは逆に）
    - popQのMaybe版（unconsQ）を追加
    - execQをインライン化しやすくなるように実装修正・各関数にINLINE追加
    - showの表示をリスト形式に変更

--- src/My/Data/Queue.hs
+++ src/My/Data/Queue.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ViewPatterns #-}
 module My.Data.Queue where
 
 import qualified Data.List as L
 
 -- | 実時間キューの実装（各キュー操作の最悪計算量がすべてO(1)）
-data Queue a = Q [a] [a] [a]
+data Queue a = Q ![a] ![a] ![a]
 
 emptyQ :: Queue a
 emptyQ = Q [] [] []
@@ -11,17 +13,31 @@ emptyQ = Q [] [] []
 isEmptyQ :: Queue a -> Bool
 isEmptyQ (Q f _ _) = null f
 
-pushQ :: Queue a -> a -> Queue a
-pushQ (Q f r a) x = execQ (Q f (x:r) a)
+{-# INLINE pushQ #-}
+pushQ :: a -> Queue a -> Queue a
+pushQ x (Q f r a) = execQ (Q f (x:r) a)
 
+{-# INLINE (.>) #-}
+(.>) :: Queue a -> a -> Queue a
+q .> x = pushQ x q
+infixl 5 .>
+
+{-# INLINE popQ #-}
 popQ :: Queue a -> (a, Queue a)
 popQ (Q [] _ _) = error "pop from empty Queue."
 popQ (Q (x:f) r a) = (x, execQ (Q f r a))
 
+{-# INLINE unconsQ #-}
+unconsQ :: Queue a -> Maybe (a, Queue a)
+unconsQ (Q [] _ _) = Nothing
+unconsQ (Q (x:f) r a) = Just (x, execQ (Q f r a))
+
+{-# INLINE execQ #-}
 execQ :: Queue a -> Queue a
-execQ (Q f r (_:xs)) = Q f r xs
-execQ (Q f r []) = let f' = rotateQ f r []
-                   in Q f' [] f'
+execQ (Q f r a) = case a of
+    (_:xs) -> Q f r xs
+    [] ->let f' = rotateQ f r []
+         in Q f' [] f'
     where rotateQ :: [a] -> [a] -> [a] -> [a]
           rotateQ [] (y:_) as = y:as
           rotateQ (x:xs) (y:ys) as = x : rotateQ xs ys (y:as)
@@ -29,9 +45,9 @@ execQ (Q f r []) = let f' = rotateQ f r []
 
 -- >>> queue = fromListQ [1,2,3,4,5]
 -- >>> addListQ queue [6,7,8,9]
--- 1 :@ 2 :@ 3 :@ 4 :@ 5 :@ 6 :@ 7 :@ 8 :@ 9 :@ EmptyQ
+-- Q [1,2,3,4,5,6,7,8,9]
 addListQ :: Queue a -> [a] -> Queue a
-addListQ = L.foldl' pushQ
+addListQ = L.foldl' (.>)
 
 fromListQ :: [a] -> Queue a
 fromListQ = addListQ emptyQ
@@ -39,21 +55,18 @@ fromListQ = addListQ emptyQ
 -- >>> toListQ $ fromListQ [1,2,3,4,5]
 -- [1,2,3,4,5]
 toListQ :: Queue a -> [a]
-toListQ = L.unfoldr $ \q -> if isEmptyQ q then Nothing else Just (popQ q)
+toListQ = L.unfoldr unconsQ
 
 singletonQ :: a -> Queue a
 singletonQ x = fromListQ [x]
 
-data ViewQ a = EmptyQ | a :@ Queue a
+data ViewQ a = EmptyQ | a :@ !(Queue a)
 
 viewQ :: Queue a -> ViewQ a
-viewQ q = if isEmptyQ q then EmptyQ
-          else let (v, rest) = popQ q
-               in v :@ rest
+viewQ q = case unconsQ q of
+    Nothing -> EmptyQ
+    Just (v, rest) -> v :@ rest
 
 instance Show a => Show (Queue a) where
     show :: Show a => Queue a -> String
-    show q = case viewQ q of
-        EmptyQ -> "EmptyQ"
-        v :@ rest -> show v <> " :@ " <> show rest
-
+    show q = "Q " <> show (toListQ q)
