3ef9d7ac3f497dfc600e44c2539ace5e6ae5fda0
Takayuki Uchida <jacobitebeerlove@gmail.com>
2026-01-17 13:47:53 +0900

    unsafeFreeze/unsafeThawのラッパーを追加

--- src/My/Data/Array.hs
+++ src/My/Data/Array.hs
@@ -5,61 +5,95 @@ import Control.Monad.ST
 import Data.Array.IArray
 import Data.Array.IO
 import Data.Array.ST
+import Data.Array.Unboxed
+import Data.Array.Unsafe
 import Data.Foldable
 
 class (Monad m, MArray a e m) => MBArray a e m | m -> a where
     newBArray :: Ix i => (i, i) -> e -> m (a i e)
     newBListArray :: Ix i => (i, i) -> [e] -> m (a i e)
+    unsafeBFreeze :: (Ix i, MArray a e m) => a i e -> m (Array i e)
+    unsafeBThaw :: (Ix i, MArray a e m) => Array i e -> m (a i e)
 
 instance MBArray IOArray e IO where
     newBArray = newArray
     newBListArray = newListArray
+    unsafeBFreeze = unsafeFreeze
+    unsafeBThaw = unsafeThaw
 
 instance MBArray (STArray s) e (ST s) where
     newBArray = newArray
     newBListArray = newListArray
+    unsafeBFreeze = unsafeFreeze
+    unsafeBThaw = unsafeThaw
 
 class (Monad m, MArray a e m) => MUArray a e m | m -> a where
     newUArray :: Ix i => (i, i) -> e -> m (a i e)
     newUListArray :: Ix i => (i, i) -> [e] -> m (a i e)
+    unsafeUFreeze :: (Ix i, MArray a e m) => a i e -> m (UArray i e)
+    unsafeUThaw :: (Ix i, MArray a e m) => UArray i e -> m (a i e)
 
 instance MUArray IOUArray Bool IO where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray IOUArray Char IO where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray IOUArray Double IO where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray IOUArray Float IO where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray IOUArray Int IO where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray IOUArray Word IO where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 
 instance MUArray (STUArray s) Bool (ST s) where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray (STUArray s) Char (ST s) where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray (STUArray s) Double (ST s) where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray (STUArray s) Float (ST s) where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray (STUArray s) Int (ST s) where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 instance MUArray (STUArray s) Word (ST s) where
   newUArray = newArray
   newUListArray = newListArray
+  unsafeUFreeze = unsafeFreeze
+  unsafeUThaw = unsafeThaw
 
 -- | 2次元Array用のscanl
 ascanl2dBase :: (MArray a e m, IArray b e) => (e -> e -> e) -> e -> b (Int, Int) e -> m (a (Int, Int) e)
