496069ddbc19be858988c5baf72d52b0ec67c824
Takayuki Uchida <jacobitebeerlove@gmail.com>
2026-02-06 03:44:40 +0900

    charGridで中間リストを生成しないように修正
    巨大なCharGridを読み込むときに、中間リストがあるとMLEしてしまうため。
    参考：https://atcoder.jp/contests/abc443/submissions/73015441

--- src/My/Conv/Grid.hs
+++ src/My/Conv/Grid.hs
@@ -1,10 +1,22 @@
+{-# LANGUAGE BlockArguments #-}
 module My.Conv.Grid where
 
+import Data.Array.ST
 import Data.Array.Unboxed
+import qualified Data.ByteString.Char8 as BS
+import Data.Foldable
+import qualified Data.Vector.Strict as V
 import My.Conv
+import My.Data.Array
 
 charGrid :: Int -> Int -> Conv (UArray (Int, Int) Char)
-charGrid !h !w = listArray ((1,1),(h,w)) <$> listN (h*w) ucChar
+charGrid !h !w = do
+    rows <- vector0 h ucBS
+    pure $ runSTUArray do
+        res <- newUArray_ ((1,1),(h,w))
+        for_ [1..h] \i -> for_ [1..w] \j -> do
+            writeArray res (i,j) $ (rows V.! (i-1)) `BS.index` (j-1)
+        return res
 
 intGrid :: Int -> Int -> Conv (UArray (Int, Int) Int)
 intGrid !h !w = listArray ((1,1),(h,w)) <$> listN (h*w) ucInt
