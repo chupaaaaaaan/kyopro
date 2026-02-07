397be6133d46af8800eb1a0c7665f34c9d536689
Takayuki Uchida <jacobitebeerlove@gmail.com>
2026-02-08 01:33:10 +0900

    intGridを中間リストを作らない実装に変更

--- src/My/Conv/Grid.hs
+++ src/My/Conv/Grid.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE BlockArguments #-}
 module My.Conv.Grid where
 
+import Control.Monad.ST
+import Control.Monad.State.Strict
+import Control.Monad.Trans.Maybe
 import Data.Array.ST
 import Data.Array.Unboxed
 import qualified Data.ByteString.Char8 as BS
 import Data.Foldable
 import qualified Data.Vector.Strict as V
+import qualified Data.Vector.Unboxing as VU
 import My.Conv
 import My.Data.Array
 
@@ -19,4 +23,16 @@ charGrid !h !w = do
         return res
 
 intGrid :: Int -> Int -> Conv (UArray (Int, Int) Int)
-intGrid !h !w = listArray ((1,1),(h,w)) <$> listN (h*w) ucInt
+intGrid !h !w = do
+    rows <- vector0 h ucBSLine
+    case runST (runMaybeT (buildArr rows)) of
+        Nothing -> StateT (const Nothing)
+        Just arr -> pure arr
+
+    where ucRow = vector0 w ucInt
+          buildArr rows = do
+              res <- lift $ newUArray_ ((1,1),(h,w))
+              for_ [1..h] \i -> do
+                  row <- hoistMaybe $ ucRow `evalStateT` (rows V.! (i-1))
+                  lift $ for_ [1..w] \j -> writeArray res (i,j) (row VU.! (j-1))
+              lift $ unsafeUFreeze res
